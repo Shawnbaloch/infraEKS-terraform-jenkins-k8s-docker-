587ba86a88813ed4bcd13bf3571d3b321f4aa4e4
Shah Nawaz <68929104+Shawnbaloch@users.noreply.github.com>
2024-01-07 17:53:26 -0600

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-shah-eks-backend"Replace with your actual S3 bucket name
-    key    = "EKS/terraform.tfstate"
+    bucket = "terraform-shah-eks-backend" # Replace with your actual S3 bucket name
+    key    = "path/to/terraform.tfstate"
     region = "us-east-1"
   }
 }
